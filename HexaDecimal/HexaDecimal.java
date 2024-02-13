8ff4cc2a45dabb4300bf73f542ade33707f1f9ba
Anujesh Ansh <anshanujesh@gmail.com>
2024-02-13 15:45:53 +0530

    eigth commit

---
+++ HexaDecimal/HexaDecimal.java
@@ -0,0 +1,16 @@
+// package HexaDecimal;
+
+// import java.util.Scanner;
+
+// public class HexaDecimal {
+//     public static void main(String[] args) {
+//         int x;
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a decimal number:");
+//         x = sc.nextInt();
+//         sc.close();
+//         x+=1;
+//         //Converting the given decimal number to hexadecimal
+        
+//     }
+// }
---
+++ HexaDecimal/hexadecimal.py
@@ -0,0 +1,5 @@
+import math
+x = int(input("Enter the Number:- "))
+print("Decimal No:- ",x)
+print("HexaDecimal No:- ",hex(x))
+print("OctaDecimal No:- ",oct(x) )
