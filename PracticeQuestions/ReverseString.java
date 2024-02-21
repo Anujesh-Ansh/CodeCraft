0eba306784fb0f5035edda6c8fe1f363d1ca8b5e
Anujesh Ansh <anshanujesh@gmail.com>
2024-02-21 10:54:12 +0530

    practice question 7

--- PracticeQuestions/ReverseString.java
+++ PracticeQuestions/ReverseString.java
@@ -4,7 +4,24 @@
  */
 
 package PracticeQuestions;
+import java.util.*;
 
 public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine(); // input daalo idhar
+        String[] arr = s.split(" ");
+        int x = arr.length;
+        for(int i=0;i<x;i++){
+            int a = arr[i].length();
+            for(int j=a-1;j>=0;j--){
+                System.out.print(arr[i].charAt(j));
+            }
+            System.out.print(" ");
+        }
+        System.out.println();
+
+        sc.close();
+    }
     
 }
