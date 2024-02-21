44ab349612d0ad2ad5e41b38fdd9af91cea85500
Anujesh Ansh <anshanujesh@gmail.com>
2024-02-21 10:45:29 +0530

    practice question 7

--- PracticeQuestions/ReverseString.java
+++ PracticeQuestions/ReverseString.java
@@ -1,3 +1,8 @@
+/*
+ * You are given a sentence as a string. You are required to reverse the words of
+ * that sentence alternatively starting from the first word.
+ */
+
 package PracticeQuestions;
 
 public class ReverseString {
